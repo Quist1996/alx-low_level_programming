a956f8b27c3c617584571ea44efd2d90b15d358c
Daniel <danielquist224@gmail.com>
2023-10-09 10:36:26 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-/**
+/*
  * main - Entry point for the program
  *
- * Description: prints a line with proper grammar using the printf function
+ * Description: This function prints the sizes of various data types in bytes.
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c)); /* Corrected size of long int */
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); /* Corrected size of float */
+
 return (0);
 }
-
