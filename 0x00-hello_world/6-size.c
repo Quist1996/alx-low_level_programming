6ad6c356cc73d17825c5e127e1e550a27aafa17c
Daniel <danielquist224@gmail.com>
2023-10-09 10:17:44 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description: prints a line with proper grammar using the printf function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
