642dbfadbff7a918efeab55cfb52490323ab511b
Daniel <danielquist224@gmail.com>
2023-10-11 13:53:19 -0700

    betty 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  add - adds two integers and returns the result.
+ *  @x: is the first parameter
+ *  @y: is the second parameter
+ *  Return: value of function
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
